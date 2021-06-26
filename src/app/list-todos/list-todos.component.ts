85456510a16628390d1e35376ab82bc5005cebd9
sandyrai <kmr.sandeep1997@gmail.com>
2021-06-27 03:22:07 +0530

    menu and footer added

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,11 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo{
+  constructor(public id:number,
+    public description:string,
+    public done:boolean,
+    public targetDate:Date
+    ){
+
+    }
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
   styleUrls: ['./list-todos.component.css']
 })
 export class ListTodosComponent implements OnInit {
+
+
+   todos =[
+    new Todo(1,'Learn to Dance',false,new Date()),
+    new Todo(2,'Learn to Sing',false,new Date()),
+    // {id:3,description:'Trip to Goa'},
+    // {id:4,description:'Hill trip'},
+   ]
+
 
   constructor() { }
 
